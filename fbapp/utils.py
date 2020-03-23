f12edb45120a5f36ac1f2c37b7ad20238306e635
sabiarist <aristide.sabi@innovgroup.org>
2020-03-23 05:22:20 +0000

    Installation de Pillow, Generation d'une image de fond

--- fbapp/utils.py
+++ fbapp/utils.py
@@ -1,7 +1,28 @@
 import random
+from PIL import Image
+
+"""
 from fbapp.models import Content, Gender
 
 
 def find_content(gender):
     contents = Content.query.filter(Content.gender == Gender[gender]).all()
     return random.choice(contents)
+"""
+
+
+class OpenGraphImage:
+    def __init__(self, first_name, description):
+        background = self.base()
+        background.show()
+
+    @staticmethod
+    def base():
+        img = Image.new('RGB', (1200, 630), '#18BC9C')
+        return img
+
+
+description = """
+    Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)
+    """
+OpenGraphImage('Celine', description)
