1d88820b37fd75a1390c5e04c7638e5edf4ab686
sabiarist <aristide.sabi@innovgroup.org>
2020-03-29 05:49:37 +0000

    Test de l'authentification sur facebook: User voit le formulaire

--- fbapp/tests/test_user_takes_the_test.py
+++ fbapp/tests/test_user_takes_the_test.py
@@ -11,6 +11,7 @@ class TestUserTakesTheTest(LiveServerTestCase):
     def test_user_login(self):
         self.driver.get(self.get_server_url())
         self.clicks_on_Login()
+        self.sees_login_page()
         assert self.driver.current_url == 'http://localhost:8943/'
 
     def create_app(self):
@@ -33,6 +34,19 @@ class TestUserTakesTheTest(LiveServerTestCase):
         self.wait.until(lambda driver: self.driver.find_element_by_tag_name("iframe").is_displayed())
         # Clique sur l'element
         ActionChains(self.driver).click(button).perform()
+
+    def sees_login_page(self):
+        # On attend d'avoir plus d'une fenêtre ouverte.
+        self.wait.until(lambda driver: len(self.driver.window_handles) > 1)
+        # switch_to permet de changer de fenêtre.
+        # window_handles renvoie une liste contenant toutes les fenêtres ouvertes
+        # par ordre d'ouverture.
+        # La fenêtre d'authentification étant la dernière ouverte,
+        # c'est la dernière de la liste.
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        # On attend que la page ait fini de charger.
+        self.wait.until(lambda driver: self.get_el('#email'))
+        assert self.driver.current_url.startswith('https://www.facebook.com/login.php')  # <3 Python
 
     def tearDown(self):
         self.driver.quit()
